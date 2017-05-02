557a5d929d6bc83786991f31c81177ec09b9739d
Karin Sevegnani <karin.sevegnani22@gmail.com>
2017-05-02 17:20:03 +0200

    stories and modal

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('#crea-att').modal();
 
+  $('#rate-act').modal();
+
   $('#modifica').modal();
 
   $('#crea-gruppo').modal();
