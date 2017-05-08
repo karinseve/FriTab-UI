3de0e7de65dae08dc425b9fbac61be2451032a23
Karin Sevegnani <karin.sevegnani22@gmail.com>
2017-05-08 11:04:59 +0200

    url

--- js/main.js
+++ js/main.js
@@ -392,7 +392,7 @@ var actDetailsData = {
         createCookie('name', name, 1);
         createCookie('sesso', sesso, 1);
         myName = readCookie('name');
-        window.location = 'karinseve.github.io/Acanto/activities.html'
+        window.location = 'activities.html'
       }
   });
 
@@ -403,7 +403,7 @@ var actDetailsData = {
       eraseCookie('name');
       eraseCookie('sesso');
       myName=null;
-      window.location = 'karinseve.github.io/Acanto/initial.html'
+      window.location = 'initial.html'
     }
   });
 
